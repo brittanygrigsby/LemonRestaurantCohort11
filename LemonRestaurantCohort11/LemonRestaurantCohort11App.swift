15e63c985251b144169fc99a7c0987c5eed29619
brittanygrigsby <brittanysugrigsby@gmail.com>
2026-07-13 15:46:02 -0700

    Initial Commit

---
+++ LemonRestaurantCohort11/LemonRestaurantCohort11App.swift
@@ -0,0 +1,17 @@
+//
+//  LemonRestaurantCohort11App.swift
+//  LemonRestaurantCohort11
+//
+//  Created by Brittany Grigsby on 7/13/26.
+//
+
+import SwiftUI
+
+@main
+struct LemonRestaurantCohort11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
