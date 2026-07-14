771430d1e3025ff563e420d20e42a5be3c855403
brittanygrigsby <brittanysugrigsby@gmail.com>
2026-07-14 12:52:53 -0700

    update

---
+++ LemonRestaurantCohort11/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  LemonRestaurantCohort11
+//
+//  Created by Brittany Grigsby on 7/13/26.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
