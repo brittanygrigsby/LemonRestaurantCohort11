e9b28c2d1c7e1de00b0ba1c0064e8e9a97e57a3b
brittanygrigsby <brittanysugrigsby@gmail.com>
2026-07-18 12:16:49 -0700

    update drink size

---
+++ LemonRestaurantCohort11/DrinkSizeSwitcher.swift
@@ -0,0 +1,18 @@
+//
+//  DrinkSizeSwitcher.swift
+//  LemonRestaurantCohort11
+//
+//  Created by Brittany Grigsby on 7/18/26.
+//
+
+import SwiftUI
+
+struct DrinkSizeSwitcher: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DrinkSizeSwitcher()
+}
