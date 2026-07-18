ea95f70bb1fbfd3d65c3b55f80cfd9f09d190c2d
brittanygrigsby <brittanysugrigsby@gmail.com>
2026-07-18 14:12:05 -0700

    fav food list

---
+++ LemonRestaurantCohort11/DataListView.swift
@@ -0,0 +1,18 @@
+//
+//  DataListView.swift
+//  LemonRestaurantCohort11
+//
+//  Created by Brittany Grigsby on 7/18/26.
+//
+
+import SwiftUI
+
+struct DataListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DataListView()
+}
