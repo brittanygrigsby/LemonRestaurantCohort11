9f68bfbf95827cff56fa1bbfe8263364e8fe6b94
brittanygrigsby <brittanysugrigsby@gmail.com>
2026-07-25 11:55:09 -0700

    update menuview

---
+++ LemonRestaurantCohort11/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  LemonRestaurantCohort11
+//
+//  Created by Brittany Grigsby on 7/24/26.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
