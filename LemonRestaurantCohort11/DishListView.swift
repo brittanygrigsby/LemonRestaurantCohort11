5d1b9149ca7b3f29707188eac9fcad13f91d060b
brittanygrigsby <brittanysugrigsby@gmail.com>
2026-08-02 23:24:48 -0700

    dishlistview

---
+++ LemonRestaurantCohort11/DishListView.swift
@@ -0,0 +1,18 @@
+//
+//  DishListView.swift
+//  LemonRestaurantCohort11
+//
+//  Created by Brittany Grigsby on 8/2/26.
+//
+
+import SwiftUI
+
+struct DishListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DishListView()
+}
---
+++ LemonRestaurantCohort11/Models/Dish.swift
@@ -0,0 +1,8 @@
+//
+//  Dish.swift
+//  LemonRestaurantCohort11
+//
+//  Created by Brittany Grigsby on 8/2/26.
+//
+
+import Foundation
