3c12ef248dd8bd50731324e65c573de69d944217
brittanygrigsby <brittanysugrigsby@gmail.com>
2026-08-03 07:21:34 -0700

    adddishview

---
+++ LemonRestaurantCohort11/AddDishView.swift
@@ -0,0 +1,18 @@
+//
+//  AddDishView.swift
+//  LemonRestaurantCohort11
+//
+//  Created by Brittany Grigsby on 8/3/26.
+//
+
+import SwiftUI
+
+struct AddDishView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddDishView()
+}
